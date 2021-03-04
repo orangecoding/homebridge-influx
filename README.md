8586beb6833f0e4ed0855e7bd072f4ab9f64d641
orangecoding <weakmap@gmail.com>
2021-03-04 15:29:24 +0100

    initial commi

---
+++ README.md
@@ -0,0 +1,81 @@
+# homebridge-influx ![npm](https://img.shields.io/npm/v/homebridge-influx?style=flat-square)
+
+A Homebridge plugin that exposes temperature and humidity from an InfluxDB instance. It is collecting always the latest value from a measurement.
+
+
+## Install
+
+Install the plugin using:
+
+```bash
+npm i -g homebridge-influx
+```
+
+
+## Configure
+
+Add to the `accessories` field of your Homebridge `config.json` file:
+
+```
+{
+  ...
+  "accessories": [
+      ...
+      {
+        "accessory": "Homebridge-Influx",
+        "name": "SomeName",  // Name for the sensors
+
+        // Optional names for each sensor
+        "sensor_names": {
+          "temperature": "Temperature Sensor",
+          "humidity": "Humidity Sensor"
+        },
+
+        // For influxDB queries
+        "schema": {
+          "temperature": {
+            "field": "temperature",
+            "measurement": "air"
+          },
+          "humidity": {
+            "field": "humidity",
+            "measurement": "air"
+          }
+        },
+        "influx": {
+          "host": "127.0.0.1",
+          "database": "homeserver"
+        }
+      }
+    ]
+}
+```
+
+The `influx` configuration object is passed as-is to the `influx` npm library, so you can use all the options it supports. 
+See [here](https://node-influx.github.io/class/src/index.js~InfluxDB.html#instance-constructor-constructor)
+
+# Influx 2
+
+This plugin is also compatible with Influx 2, however in order to make it work, you need to do 2 things.
+
+### Configuration
+
+In the `influx` part of the configuration, you need to parse in the token.
+
+```json
+        "host": "yourHost.com",
+        "port": "yourPort",
+        "database": "someBucket", //type in the name of the bucket here
+        "protocol": "http",
+        "password": null,
+        "options": {
+            "headers": {
+                "Authorization": "Token YOUR_TOKEN"
+            }
+        }
+```
+
+A bucket is mapped to a database. In order to make Influx aware of this mapping, you need to create it. You do this via terminal by running:
+```bash
+influx v1 dbrp create --db *BUCKET_NAME* --rp *BUCKET_NAME* --bucket-id *BUCKET_ID* --default
+```
---
+++ src/index.ts
@@ -0,0 +1,102 @@
+let Service, Characteristic;
+
+import InfluxDB = require('influx');
+
+const defaultConfig = {
+    sensor_names: {
+        temperature: 'Temperature Sensor',
+        humidity: 'Humidity Sensor',
+    },
+    schema: {
+        temperature: { field: 'temperature', measurement: 'air' },
+        humidity: { field: 'humidity', measurement: 'air' }
+    }
+};
+
+const getLastMesurement = (influx, service, schema, cb) => {
+    influx
+        .query(`SELECT LAST("${schema[service].field}") FROM ${schema[service].measurement}`)
+        .then(result => cb(null, result[0].last))
+        .catch(err => cb(err));
+};
+
+const round = (value, decimal = 0) => {
+    return Math.round((value + Number.EPSILON) * 10 ** decimal) / 10 ** decimal;
+};
+
+module.exports = homebridge => {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory('homebridge-influx', 'Homebridge-Influx', HomebridgeInflux);
+};
+
+function HomebridgeInflux(log, config) {
+    this.log = log;
+
+    // Configuration
+    this.name = config['name'];
+    this.manufacturer = config['manufacturer'] || 'Home';
+    this.model = config['model'] || 'InfluxDB';
+    this.serial = config['serial'] || '1';
+
+    this.sensor_names = { ...defaultConfig['sensor_names'], ...config['sensor_names'] };
+    this.schema = { ...defaultConfig['schema'], ...config['schema'] };
+
+    this.influx = new InfluxDB.InfluxDB({ ...config['influx'] });
+}
+
+HomebridgeInflux.prototype = {
+    // Called when HomeKit wants to read our sensor value.
+    getRemoteState: function (service, callback) {
+        getLastMesurement(
+            this.influx,
+            service,
+            this.schema,
+            function (influxError, value) {
+
+                if (influxError) {
+                    this.log(influxError);
+                    return callback(new Error(influxError));
+                }
+                const v = round(value, 1);
+                this.temperatureService.setCharacteristic(Characteristic.Name, this.sensor_names[service]);
+                this.temperatureService.setCharacteristic(service === 'temperature' ? Characteristic.CurrentTemperature : Characteristic.CurrentRelativeHumidity, v);
+                return callback(null, v);
+            }.bind(this)
+        );
+    },
+
+    // Homekit-specific getters
+    getTemperatureState: function (callback) {
+        this.getRemoteState('temperature', callback);
+    },
+    getHumidityState: function (callback) {
+        this.getRemoteState('humidity', callback);
+    },
+
+    // Service configuration
+    // Sets up all the capacities of the accessory, as well as the AccessoryInformation, which provides basic identification for our accessory.
+    getServices: function () {
+        const informationService = new Service.AccessoryInformation();
+
+        this.temperatureService = new Service.TemperatureSensor(this.name);
+        this.humidityService = new Service.HumiditySensor(this.name);
+
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+            .setCharacteristic(Characteristic.Model, this.model)
+            .setCharacteristic(Characteristic.SerialNumber, this.serial);
+
+        this.temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .setProps({ minValue: -273, maxValue: 200 })
+            .on('get', this.getTemperatureState.bind(this));
+
+        this.humidityService
+            .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+            .setProps({ minValue: 0, maxValue: 100 })
+            .on('get', this.getHumidityState.bind(this));
+
+        return [informationService, this.temperatureService, this.humidityService];
+    }
+};
